ae8224cdb345d84e77695c1508abc1166b18db63
Stephan <stepannk@mail.ru>
2018-11-18 21:51:20 +0300

    Working tracking system with Haar classifier

--- src/main/java/TrackingSystem.java
+++ src/main/java/TrackingSystem.java
@@ -1,12 +1,12 @@
 import io.reactivex.Observable;
 import org.bytedeco.javacpp.avcodec;
 import org.bytedeco.javacpp.avutil;
-import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacv.FFmpegFrameRecorder;
+import ru.zuma.rx.RxAsyncVideoConsumer;
 import ru.zuma.rx.RxClassifier;
-import ru.zuma.rx.RxVideoConsumer;
 import ru.zuma.rx.RxVideoSource2;
 import ru.zuma.utils.ConsoleUtil;
+import ru.zuma.utils.ImageMarker;
 import ru.zuma.utils.Pair;
 import ru.zuma.utils.ResourceLoader;
 import ru.zuma.video.VideoConsumer;
@@ -16,57 +16,103 @@ import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
 
 import static org.bytedeco.javacpp.avformat.av_register_all;
+import static org.bytedeco.javacpp.opencv_core.*;
 
 public class TrackingSystem {
+    private Servo servoUD;
+    private Servo servoLR;
+    private float fieldOfView;
+
+    //
+    // Threshold of correcting angles as part of rect size
+    private float sensitivity;
+
+    public TrackingSystem() {
+        this(70);
+    }
+
+    public TrackingSystem(int fieldOfViewDeg) {
+        this.servoLR = new Servo(1);
+        this.servoUD = new Servo(0);
+        this.fieldOfView = fieldOfViewDeg / 180f * (float)Math.PI;
+        this.sensitivity = 0.2f;
+    }
 
     public static void main(String[] args) {
         try {
-//            avutil.av_log_set_level(avutil.AV_LOG_ERROR);
             av_register_all();
             run(args);
+            Thread.sleep(2000);
+            avutil.av_log_set_level(avutil.AV_LOG_ERROR);
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     protected static void run(String[] args) throws IOException {
-        try {
-            ResourceLoader.configureFromClass(TrackingSystem.class);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+        TrackingSystem trackingSystem = new TrackingSystem();
 
+
+        ResourceLoader.configureFromClass(TrackingSystem.class);
         RxVideoSource2 videoSource = ConsoleUtil.createVideoSource(args);
         RxClassifier classifier = ConsoleUtil.createClassifier();
-        RxVideoConsumer videoConsumer = createVideoConsumer(args);
-        Observable<Pair<opencv_core.Mat, opencv_core.RectVector>> classifierPairObserver;
+        RxAsyncVideoConsumer videoConsumer = createVideoConsumer(args);
+        Observable<Pair<Mat, RectVector>> classifierPairObserver;
 
         videoSource
-                .throttleFirst(2000, TimeUnit.MILLISECONDS)
+                .throttleFirst(200, TimeUnit.MILLISECONDS)
                 .subscribe(classifier);
 
-        FPSCounter counter = new FPSCounter();
+        FPSCounter cameraFPS = new FPSCounter(20);
+        FPSCounter recorderFPS = new FPSCounter(20);
         videoSource.subscribe(mat -> {
-            counter.countdown();
+            cameraFPS.countdown();
         });
 
         videoSource
-                .throttleFirst(1000, TimeUnit.MILLISECONDS)
+                .throttleFirst(1500, TimeUnit.MILLISECONDS)
                 .subscribe(mat -> {
-                    System.out.println("FPS:" + counter.getFPS());
+                    System.out.println("Camera FPS: " + cameraFPS.getFPS() + ", Recorder FPS: " + recorderFPS.getFPS());
                 });
 
         classifierPairObserver = Observable.combineLatest(
                 videoSource, classifier,
                 Pair::new
         );
+
+        // Control servos
         classifierPairObserver
-                .map((pair) -> pair.first())
-                .subscribe(videoConsumer);
+                .throttleFirst(200, TimeUnit.MILLISECONDS)
+                .subscribe(pair -> {
+                    if (pair.second().empty()) return;
+
+                    Rect largerRect = pair.second().get(0);
+                    for (int i = 0; i < pair.second().size(); i++) {
+                        Rect curRect = pair.second().get(i);
+                        if (largerRect.width() + largerRect.height() < curRect.width() + curRect.height()) {
+                            largerRect = curRect;
+                        }
+                    }
+                    trackingSystem.rotateToCenter(pair.first(), largerRect);
+                });
+
+        // Record stream
+        Mat markedImage = new Mat();
+        classifierPairObserver
+                .subscribe(pair -> {
+                    if (videoConsumer != null && videoConsumer.isReadyForNext()) {
+                        pair.first().copyTo(markedImage);
+                        ImageMarker.markRects(markedImage, pair.second());
+                        videoConsumer.onNext(markedImage);
+                        recorderFPS.countdown();
+                    }
+                });
     }
 
-    protected static RxVideoConsumer createVideoConsumer(String[] args) {
+    protected static RxAsyncVideoConsumer createVideoConsumer(String[] args) {
         VideoConsumer consumer;
         FFmpegFrameRecorder recorder;
 
@@ -89,8 +135,8 @@ public class TrackingSystem {
 //        recorder.setVideoBitrate(2 * 1024 * 1024);
         recorder.setFrameRate(15);
 //        recorder.setInterleaved(true);
-        recorder.setVideoBitrate(10*1024*1024);
-        recorder.setPixelFormat(avutil.AV_PIX_FMT_YUVJ422P);
+        recorder.setVideoBitrate(4*1024*1024);
+        recorder.setPixelFormat(avutil.AV_PIX_FMT_YUVJ420P);
         recorder.setVideoCodec(avcodec.AV_CODEC_ID_MJPEG);
 //        recorder.setFrameRate(30);
 
@@ -98,11 +144,31 @@ public class TrackingSystem {
 
         if (consumer.isOpened()) {
             System.out.println("Video consumer successfully started!");
-            return new RxVideoConsumer(consumer);
+            return new RxAsyncVideoConsumer(consumer);
         } else {
             System.out.println("Video consumer starting failed. Please launch ffserver with feed http://localhost:8090/camera.ffm");
             consumer.release();
             return null;
         }
+    }
+
+    public void rotateToCenter(Mat img, Rect rect) {
+        // Calculate center of tracked rect
+        float xCenter = rect.x() + rect.width() / 2f;
+        float yCenter = rect.y() + rect.height() / 2f;
+
+        // Calculate dif between center of rect and image
+        float dx = xCenter - img.cols() / 2f;
+        float dy = yCenter - img.rows() / 2f;
+
+        if (Math.abs(dx) < rect.width() * sensitivity &
+            Math.abs(dy) < rect.height() * sensitivity) return;
+
+        // Calculate angular diff
+        float azim = dx / img.cols() * fieldOfView;
+        float elev = dy / img.rows() * fieldOfView;
+
+        servoLR.rotate(-azim);
+        servoUD.rotate(elev);
     }
 }
